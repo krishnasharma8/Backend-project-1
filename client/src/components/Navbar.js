87cb02bf3cea3e142d9f04271970ea28f90089f0
krishna <krishna520.be22@chitkara.edu.in>
2024-11-15 18:27:52 +0530

    Login and Register Updated

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React from "react";
 
 function Navbar() {
+  const user = JSON.parse(localStorage.getItem("currentUser"));
+  function logout(){
+    localStorage.removeItem('currentUser');
+    window.location.href='/login';
+  }
   return (
+     
     <div>
       <nav className="navbar navbar-expand-lg">
-        <div className="container-fluid"> {/* Changed `class` to `className` */}
+        <div className="container-fluid">
+          {" "}
+          {/* Changed `class` to `className` */}
           <a className="navbar-brand" href="/home">
             STAY-HERE
           </a>
@@ -17,20 +25,51 @@ function Navbar() {
             aria-expanded="false"
             aria-label="Toggle navigation"
           >
-            <span className="navbar-toggler-icon"></span>
+            <span className="navbar-toggler-icon" style={{color:'white'}}></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarNav">
             <ul className="navbar-nav">
-              <li className="nav-item"> {/* Removed the `active` class for simplicity */}
-                <a className="nav-link" href="/register">
-                  Register
-                </a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/login">
-                  Login
-                </a>
-              </li>
+              {user ? (
+                <>
+                  <div class="dropdown">
+                    <button
+                      class="btn btn-secondary dropdown-toggle"
+                      type="button"
+                      data-bs-toggle="dropdown"
+                      aria-expanded="false"
+                    >
+                     <i class="fa fa-user"></i>{user.name}
+                    </button>
+                    <ul class="dropdown-menu">
+                      <li>
+                        <a class="dropdown-item" href="#">
+                          Bookings
+                        </a>
+                      </li>
+                      <li>
+                        <a class="dropdown-item" href="#" onClick={logout}>
+                          Logout 
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </>
+              ) : (
+                <>
+                  <li className="nav-item">
+                    {" "}
+                    {/* Removed the `active` class for simplicity */}
+                    <a className="nav-link" href="/register">
+                      Register
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="/login">
+                      Login
+                    </a>
+                  </li>
+                </>
+              )}
             </ul>
           </div>
         </div>

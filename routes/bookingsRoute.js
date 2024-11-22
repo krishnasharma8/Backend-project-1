3040ef455030b91e954138a8732ef71b4ca7a0e0
HarshGuleri <harsh379.be22@chitkara.edu.in>
2024-11-22 11:17:40 +0530

    booking routes

---
+++ routes/bookingsRoute.js
@@ -0,0 +1,36 @@
+
+const express = require("express");
+const router = express.Router();
+const Booking = require("../models/booking");
+
+router.post("/bookroom", async (req, res) => {
+    const {
+        room,
+        userid,
+        fromdate,
+        todate,
+        totalAmount,
+        totalDays
+    } = req.body;
+
+    try {
+        const newBooking = new Booking({
+            room: room.name,
+            roomid: room._id,
+            userid,
+            fromdate,
+            todate,  // Ensure todate is included
+            totalAmount,
+            totalDays,
+            transactionId: '1234' // Placeholder for transaction ID
+        });
+
+        const booking = await newBooking.save();
+        res.status(201).json(booking); // Send back the created booking
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Error while booking the room", error: error.message }); // Send error message
+    }
+});
+
+module.exports = router;
